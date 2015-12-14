041b36f6878343857c62460b280d9b860599da4f
Karin Touchie <ktouchie@gmail.com>
2015-12-14 10:21:00 +0100

    Use .abs() method in for loop to find biggest difference.

---
+++ Exercice4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Exercice4 {
+    
+    public static void main(String [] args) {  
+
+        int a[] = {2,5,3,1,4,-7,6};
+        int bigDiff = 0;
+        
+        for (int i=0; i< a.length; i++) {
+            int diff = Math.abs(a[i]-a[i+1]);
+            if (diff>bigDiff) {
+                bigDiff = diff;
+            }
+            
+        }
+        
+        System.out.println("Le plus grand ecart entre deux entiers consecutifs dans le tableau {2,5,3,1,4,-7,6} est " + bigDiff);
+    }
+}
